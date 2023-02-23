7089f1fc3ec6d91373111aa17292fa5123ff13b3
lmlouis <louiswora1@gmail.com>
2023-02-23 20:26:22 +0100

    Configurez une page de connexion

---
+++ src/main/java/dev/lmlouis/login/controller/LoginController.java
@@ -0,0 +1,21 @@
+package dev.lmlouis.login.controller;
+
+import javax.annotation.security.RolesAllowed;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @RequestMapping("/*")
+    @RolesAllowed("USER")
+    public String getUser(){
+        return "Welcome, USER";
+    }
+
+    @RequestMapping("/admin")
+    @RolesAllowed("ADMIN")
+    public String getAdmin(){
+        return "Welcome, ADMIN";
+    }
+}
