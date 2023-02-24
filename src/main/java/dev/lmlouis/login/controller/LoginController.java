13f93ae6639fc204b249ed70f2a8aea2037e4ea9
lmlouis <louiswora1@gmail.com>
2023-02-24 02:27:25 +0100

    Authentification avec github & Explorez le Principal User

--- src/main/java/dev/lmlouis/login/controller/LoginController.java
+++ src/main/java/dev/lmlouis/login/controller/LoginController.java
@@ -4,10 +4,10 @@ import javax.annotation.security.RolesAllowed;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.security.Principal;
 @RestController
 public class LoginController {
-    @RequestMapping("/*")
+    @RequestMapping("/**") // tout appel qui ne vise pas le slach ou le slach admin soit dirigé vers le welcom github user
     @RolesAllowed("USER")
     public String getUser(){
         return "Welcome, USER";
@@ -17,5 +17,10 @@ public class LoginController {
     @RolesAllowed("ADMIN")
     public String getAdmin(){
         return "Welcome, ADMIN";
+    }
+
+    @RequestMapping("/*")
+    public String getGitHub(Principal user){
+        return user.toString();
     }
 }
